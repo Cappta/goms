ffa3eb7e64268f3ec41268b5c600f2b2ac14b1f9
Sérgio Fonseca <sergio.fonseca@cappta.com.br>
2017-09-19 15:16:27 -0300

    MAE101 PR Commented handler expected behavior

--- handler.go
+++ handler.go
@@ -2,4 +2,5 @@ package goms
 
 import "github.com/Cappta/gohelpgabs"
 
+// handler is expected alter the container and the result will be considered the response
 type handler func(container *gohelpgabs.Container)
